d729492bd03ebee795e4b85418f1a34a87c43fc1
Nafizulhaque535 <codeglobal35@gmail.com>
2020-08-09 08:44:05 +0600

    ... added

---
+++ threeDots.js
@@ -0,0 +1,13 @@
+const ages = [11, 14, 16, 13, 17];
+const ages2 = [15, 16, 12];
+const ages3 = [25, 36, 22, 29];
+const allAges = ages.concat(ages2).concat([5]).concat(ages3);
+const allAges2 = [...ages, ...ages2, 5, ...ages3];
+// console.log(allAges2);
+const business = 650;
+const minister = 450;
+const pm = 250;
+const money = [6520, 450, 250];
+// const maximum = Math.max(business, minister, pm);
+const maximum = Math.max(...money);
+console.log(maximum);
