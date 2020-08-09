4550dcd7b641e8f5d37d4baadd8c7ae4adf8865b
Nafizulhaque535 <codeglobal35@gmail.com>
2020-08-09 08:53:42 +0600

    js class added

---
+++ class.js
@@ -0,0 +1,13 @@
+class student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "UIU"
+    }
+}
+
+const student1 = new student(12, "Nafiz");
+const student2 = new student(10, "Haque");
+const student3 = new student(30, "Nabid");
+
+console.log(student3);
