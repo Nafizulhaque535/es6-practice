d2f48c41065042d31092c895ddba55dcbd51378c
Nafizulhaque535 <codeglobal35@gmail.com>
2020-08-09 08:03:04 +0600

    arrow added

---
+++ arrow.js
@@ -0,0 +1,22 @@
+// function doubleIt(num){
+//     return num * 2;
+// }
+
+// const doubleIt = function(num){
+//     return num * 2;
+// }
+
+const doubleIt = num => num * 2;
+const add = (x, y) => x + y;
+const give5 = () => 5;
+const math = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    return result;
+}
+
+const result = add(50, 70);
+const result2 = give5();
+const result3 = math(7, 5);
+console.log(result3);
